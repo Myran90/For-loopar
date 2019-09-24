2c424e627c00537e3072ea0819dbd2a828d35952
Myran <myrankalander@gmail.com>
2019-09-24 09:48:25 +0200

    Övning 9-10 klar

---
+++ övning10-for-loopar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace övning10_for_loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 40; i > 29; i--)
+            {
+			Console.WriteLine(i);
+		    }
+        }
+    }
+}
---
+++ övning9-for-loopar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace övning9_for_loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+		    {
+			Console.WriteLine(i);
+		    }
+        }
+    }
+}
